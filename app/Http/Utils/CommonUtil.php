1458ed382703f1d1e851f64e9047554d5a8f254a
accacc <accacc@126.com>
2017-06-10 23:26:46 +0800

    1111

--- app/Http/Utils/CommonUtil.php
+++ app/Http/Utils/CommonUtil.php
@@ -4,12 +4,17 @@ namespace App\Http\Utils;
 class CommonUtil{
 	public static function page_title($url) {
         $fp = file_get_contents($url);
-        if (!$fp) 
-            return null;
+        if (!$fp) {
+        	\Log::info("can not open".$url);
+        	return null;
+        }
 
         $res = preg_match("/<title>(.*)<\/title>/siU", $fp, $title_matches);
-        if (!$res) 
-            return null; 
+        if (!$res) {
+        	\Log::info("can not preg".$url);
+        	return null;
+        }
+            
 
         // Clean up title: remove EOL's and excessive whitespace.
         $title = preg_replace('/\s+/', ' ', $title_matches[1]);
