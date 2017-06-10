38364c47637d395d474a55b0909a7e1e7b8c352c
accacc <accacc@126.com>
2017-06-10 23:50:14 +0800

    123

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -14,7 +14,7 @@
     					<ul>
     					@foreach($category->feeds as $feed)
     						<li>
-    							<a href="{{ url('feeds?feed_id='.$feed->id.'&status'.($request->has('status')?$request->status:'unread')) }}">{{$feed->feed_name}}</a>
+    							<a href="{{ url('feeds?feed_id='.$feed->id.'&status='.$status }}">{{$feed->feed_name}}</a>
     							(
     							@if($status == 'unread')
     								{{$feed->unread_count}}
