e206d2913ff6e55b5991697531b42cc68879f08d
accacc <accacc@126.com>
2017-06-11 10:35:33 +0800

    pu

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -24,7 +24,7 @@ class StatisticsController extends Controller
      * @param  TaskRepository  $tasks
      * @return void
      */
-    public function __construct(StatisticsRepository $notes)
+    public function __construct(StatisticsRepository $statistics)
     {
         $this->middleware('auth');
 
