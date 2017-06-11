afd1df1c026aa88bfbda45ffe4d2edb54d894fb9
accacc <accacc@126.com>
2017-06-11 14:21:25 +0800

    up

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -49,7 +49,7 @@ function html_encode(str)
 
 @section('content')
     <div class="container">
-        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-sm-offset-0 col-sm-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     	新的笔记
