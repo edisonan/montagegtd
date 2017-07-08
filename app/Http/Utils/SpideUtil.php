5aa3499dc3a6d340750e488f6aff1a37f04cb4ac
accacc <accacc@126.com>
2017-07-09 01:37:03 +0800

    uuu

--- app/Http/Utils/SpideUtil.php
+++ app/Http/Utils/SpideUtil.php
@@ -115,7 +115,7 @@ class SpideUtil{
 			$args[] = '/images/temp/';
 		}
 		
-		$content = str_replace($img_urls, array_map('\App\Utils\SpideUtil::get_image_filename', $img_urls, $args), $content);
+		$content = str_replace($img_urls, array_map('\App\Http\Utils\SpideUtil::get_image_filename', $img_urls, $args), $content);
 		$content = str_replace('<img', '<img style="margin:0 auto;display:block;height:300px"', $content);
 		
 		foreach ($img_urls as $img_url) {
