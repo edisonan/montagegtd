04f78787ba3f21580ed5f5cd75dfffcc46ee66f9
accacc <accacc@126.com>
2017-07-09 03:20:09 +0800

    up

--- resources/views/minds/view.blade.php
+++ resources/views/minds/view.blade.php
@@ -180,6 +180,7 @@ $(document).ready(function () {
     function show_selected(){
         var selected_node = _jm.get_selected_node();
         if(!!selected_node){
+            console.log(selected_node.data);
             prompt_info('topic:'+selected_node.topic+',content:'+selected_node.data);
         }else{
             prompt_info('nothing');
