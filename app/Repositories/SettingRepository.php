153f7b864a887ae9ad43edfb37b5df0ffcf12ca1
accacc <accacc@126.com>
2017-07-08 16:00:49 +0800

    update

--- app/Repositories/SettingRepository.php
+++ app/Repositories/SettingRepository.php
@@ -17,7 +17,7 @@ class SettingRepository
     {
         return Setting::where('user_id', $user->id)
                     ->orderBy('created_at', 'desc')
-                    ->get();
+                    ->first();
     }
     
     /**
