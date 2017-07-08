c65044914a3bde963fb18d155b66ed79496be50c
accacc <accacc@126.com>
2017-07-08 16:08:38 +0800

    update

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -81,7 +81,7 @@ class SettingController extends Controller
     		$this->authorize('destroy', $setting);
     	}
     	
-    	$setting->save($request);
+    	$setting->save($request->attributes);
 
         if ($request->ajax() || $request->wantsJson()) {
         	$resp = $this->responseJson(self::OK_CODE);
