6cc3b67ee560007892f6e6a870c277a6f3377538
accacc <accacc@126.com>
2017-07-11 01:31:40 +0800

    update

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -139,6 +139,7 @@ $(document).ready(function () {
                     
                     		@if (count($articleSubs) > 0)
                     			<?php $article_sub_ids = array();?>
+                    			<?php echo count($articleSubs);exit;?>
 	                    		@foreach ($articleSubs as $articleSub)
 	                    		<?php $article = $articleSub->article;$article_sub_ids[] = $articleSub->id;?>
 	                            <article class="post">
