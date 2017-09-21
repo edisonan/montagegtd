e3ba1727affc14c062a50d7ea72568a7e22ebc5c
accacc <accacc@126.com>
2017-09-22 00:58:15 +0800

    up

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -167,7 +167,7 @@ $(document).ready(function () {
 	}
 
 	$(".category_items").click(function(){
-		$(this).find(".category_item").css("display","");
+		$(this).find(".category_item").toggle();
 	});
 });
 </script>
