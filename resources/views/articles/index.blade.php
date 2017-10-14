0f1c53622ee4ba81b327dc730f65370c6c27cf7c
accacc <accacc@126.com>
2017-10-14 15:09:50 +0800

    up

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -78,7 +78,7 @@ $(document).ready(function () {
 				if(active){
 					item.removeClass("active");
 				} else {
-					item.parent().find("active").removeClass("active");
+					item.parent().siblings().removeClass("active");
 					item.addClass("active");
 				}
 			}
