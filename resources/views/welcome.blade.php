27e2c9a28dd110995efeaec5b2fb33d49b2abbcb
accacc <accacc@126.com>
2017-10-22 22:56:10 +0800

    upup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,38 +9,38 @@
         <h3 style="padding-bottom: 20px;">Montage GTD</h3>
         <p class="lead"  style="padding-bottom: 100px;">专业GTD工具，在这里高效完成每一件事情，记录每一个想法，利用碎片化时间，提升一切内容。</p>
         <p>
-	        <a class="btn btn-lg btn-default" href="{{url('/login')}}" role="button">现在就开始吧</a>
+	        <a class="btn btn-lg btn-primary" href="{{url('/login')}}" role="button">现在就开始吧</a>
         </p>
       </div>
       
       <div class="row">
         <div class="col-md-3">
-          <img alt="" src="/img/time-is-money.png" class="">
-          <div class="">
+          <img alt="" src="/img/time-is-money.png" class="col-md-4">
+          <div class="col-md-8">
 	        <h4>番茄工作法</h4>
 	        <p>基于番茄工作法，帮助你集中注意力完成每一项待办。试过就知道这是有效的时间管理方法。</p>
           </div>
         </div>
         
         <div class="col-md-3">
-          <img alt="" src="/img/list.png" class="">
-          <div class="">
+          <img alt="" src="/img/list.png" class="col-md-4">
+          <div class="col-md-8">
 	        <h4>待办清单</h4>
           	<p>轻量级的待办列表功能，同时通过特殊语法提供 #标签、四象限重要程度、快速置顶等功能。</p>
           </div>
          </div>
          
         <div class="col-md-3">
-          <img alt="" src="/img/newspaper.png" class="">
-          <div class="">
+          <img alt="" src="/img/newspaper.png" class="col-md-4">
+          <div class="col-md-8">
 	        <h4>思想广场</h4>
           	<p>从收集想法、规划任务到专注工作、归纳分析，这里提供了完整的工作流效率管理。</p>
           </div>
           
          </div>
         <div class="col-md-3">
-          <img alt="" src="/img/monitor.png" class="">
-          <div class="">
+          <img alt="" src="/img/monitor.png" class="col-md-4">
+          <div class="col-md-8">
 	          <h4>RSS阅读</h4>
           	<p>汇总你的碎片化阅读，在这里高效完成对它的思考与记录！</p>
           </div>
