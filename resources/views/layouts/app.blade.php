c48acedc3291c044d4e0775f8a48cbcae5ecc13e
accacc <accacc@126.com>
2017-11-12 12:45:41 +0800

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
 	                    <li class="nav-item"><a class="nav-link" href="{{ url('index/feedback') }}" style="color:#E85205">添加反馈</a></li>
 	                    @endif
 	                    @if (Auth::guest())
-	                        <li class="nav-item"><a class="nav-link" href="{{ url('/login') }}" style="color:#E85205">登录/注册</a></li>
+	                        <li class="nav-item"><a class="nav-link" href="{{ url('/login') }}" style="color:#9BD6C5">登录/注册</a></li>
 	                        <!-- 
 	                        <li class="nav-item"><a class="nav-link" href="{{ url('/register') }}" style="color:#9BD6C5">注册</a></li>
 	                         -->
