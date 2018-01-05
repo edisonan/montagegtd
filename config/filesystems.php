11a34174824320ad0f30fbaa8b69d0fe67f36412
accacc <accacc@126.com>
2018-01-05 23:41:23 +0800

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,6 +47,11 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
         ],
+		
+		'admin' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+        ],
 
         'public' => [
             'driver' => 'local',
