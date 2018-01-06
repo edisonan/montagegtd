fa79c28b21ee59c6bdf4ea861106ea9a5c253961
accacc <accacc@126.com>
2018-01-06 22:26:50 +0800

    test

--- app/Admin/Controllers/FeedController.php
+++ app/Admin/Controllers/FeedController.php
@@ -76,8 +76,8 @@ class FeedController extends Controller
             $grid->id('ID')->sortable();
 
 //             $grid->user()->name('添加者');
-            $grid->feed_name('名称');
-            $grid->url('地址');
+//             $grid->feed_name('名称');
+//             $grid->url('地址');
 //             $grid->sub_count('订阅数')->sortable();
 //             $grid->is_recommend('推荐')->display(function ($is_recommend) {
 // 			    return $is_recommend?'是':'否';
