656891c958d621aa91fd4d60cbd509722df1ffe9
accacc <accacc@126.com>
2018-01-07 00:24:24 +0800

    yo

--- app/Repositories/NoteRepository.php
+++ app/Repositories/NoteRepository.php
@@ -41,7 +41,7 @@ class NoteRepository
      */
     public function forUserByStatus(User $user,$status,$need_page=false)
     {
-    	$note = Note::with(['noteTagMaps','user'])->where('status', $status)
+    	$note = Note::with(['noteTagMaps.tag','user'])->where('status', $status)
     	->orWhere('user_id', $user->id)
     	->orderBy('created_at', 'desc');
     	if($need_page){
