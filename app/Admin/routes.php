2a39ac3f40ba0f83504e90a67e85ac69eb0b3b40
accacc <accacc@126.com>
2018-01-07 13:43:09 +0800

    update

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,10 +11,27 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index');
-	$router->resource('users', UserController::class);
     $router->get('statistic', 'UserController@statistic');
+    
+	$router->resource('users', UserController::class);
     
 	$router->resource('feeds', FeedController::class);
 	$router->resource('feedsubs', FeedSubController::class);
+	
+	$router->resource('users', UserController::class);
+	
+	$router->resource('articles', ArticleController::class);
+	$router->resource('articlemarks', ArticleMarkController::class);
+	$router->resource('feedbacks', FeedbackController::class);
+	$router->resource('goals', GoalController::class);
+	$router->resource('categorys', CategoryController::class);
+	$router->resource('kindlelogs', KindleLogController::class);
+	$router->resource('minds', MindController::class);
+	$router->resource('notes', NoteController::class);
+	$router->resource('pomos', PomoController::class);
+	$router->resource('settings', settingController::class);
+	$router->resource('tasks', TaskController::class);
+	$router->resource('things', ThingController::class);
+	
 
 });
