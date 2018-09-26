849df13ce8e478dfeb4b6302785947a78dc51686
accacc <accacc@126.com>
2018-09-26 23:18:01 +0800

    change log class

---
+++ app/Utils/Log/LogUtil.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Utils\Log;
+
+class LogUtil
+{
+    // 请求token
+    public static $requestToken;
+
+    /**
+     * 获取请求token
+     *
+     */
+    public static function getRequestToken()
+    {
+        if (! self::$requestToken) {
+            self::$requestToken = substr($_SERVER['SERVER_ADDR'], - 3) . '-' . uniqid();
+        }
+        
+        return self::$requestToken;
+    }
+
+    /**
+     * 重置请求token
+     *
+     */
+    public static function resetRequestToken()
+    {
+        self::$requestToken = substr($_SERVER['SERVER_ADDR'], - 3) . '-' . uniqid();
+    }
+}
---
+++ app/Utils/Log/RequestTokenProcessor.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Utils\Log;
+
+class RequestTokenProcessor
+{
+
+    /**
+     *
+     * @param array $record
+     * @return array
+     */
+    public function __invoke(array $record)
+    {
+        $record['extra']['token'] = LogUtil::getRequestToken();
+        
+        return $record;
+    }
+}
