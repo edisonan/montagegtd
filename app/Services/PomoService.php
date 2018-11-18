9f4afec3c87f32270048d0d654207c1c645054f9
accacc <accacc@126.com>
2018-11-19 01:54:15 +0800

    增加Service

---
+++ app/Services/PomoService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+
+/**
+ * PomoService番茄相关
+ * @author edison.an
+ *
+ */
+class PomoService {
+	
+	
+    /**
+     * 
+     */
+	public function __construct() {
+	}
+	
+}
