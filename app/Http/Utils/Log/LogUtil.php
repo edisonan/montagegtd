608953fc9cef03c707f3372cee670891e5759a31
accacc <accacc@126.com>
2018-11-23 02:23:29 +0800

    更新日志

--- app/Http/Utils/Log/LogUtil.php
+++ app/Http/Utils/Log/LogUtil.php
@@ -13,7 +13,7 @@ class LogUtil
     public static function getRequestToken()
     {
         if (! self::$requestToken) {
-            self::$requestToken = substr($_SERVER['SERVER_ADDR'], - 3) . '-' . uniqid();
+            self::$requestToken = self::getServerAddr() . '-' . uniqid();
         }
         
         return self::$requestToken;
@@ -25,6 +25,15 @@ class LogUtil
      */
     public static function resetRequestToken()
     {
-        self::$requestToken = substr($_SERVER['SERVER_ADDR'], - 3) . '-' . uniqid();
+        self::$requestToken = self::getServerAddr() . '-' . uniqid();
+    }
+    
+    /**
+     * 获取请求地址
+     * @return string
+     */
+    public static function getServerAddr()
+    {
+    	return isset($_SERVER['SERVER_ADDR'])?substr($_SERVER['SERVER_ADDR'], - 3):'';
     }
 }
