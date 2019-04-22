9f7cf48beb351cf77dbc98a6ec5242016ef85a69
司马他 <accacc@126.com>
2019-04-22 11:36:25 +0800

    更新 CommonUtil.php

--- app/Http/Utils/CommonUtil.php
+++ app/Http/Utils/CommonUtil.php
@@ -5,7 +5,18 @@ use Illuminate\Support\Facades\Log;
 
 class CommonUtil{
 	public static function page_title($url) {
-        $fp = @file_get_contents($url);
+        $ch = curl_init();
+	    curl_setopt($ch, CURLOPT_URL, $url);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+	    if(strpos($url, 'https://') !== false){
+	        curl_setopt($curl_session, CURLOPT_SSL_VERIFYPEER, FALSE);
+	        curl_setopt($curl_session, CURLOPT_SSL_VERIFYHOST, FALSE);
+	    }
+	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
+	    $fp = curl_exec($ch);
+	    curl_close($ch);
+		
         if (!$fp) {
         	Log::info("can not open".$url);
         	return '';
