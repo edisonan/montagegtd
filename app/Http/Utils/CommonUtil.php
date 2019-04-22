0553fa0374c47921e097c0db88e0cb047781d4b4
司马他 <accacc@126.com>
2019-04-22 11:45:35 +0800

    更新 CommonUtil.php

--- app/Http/Utils/CommonUtil.php
+++ app/Http/Utils/CommonUtil.php
@@ -10,8 +10,8 @@ class CommonUtil{
 	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 	    if(strpos($url, 'https://') !== false){
-	        curl_setopt($curl_session, CURLOPT_SSL_VERIFYPEER, FALSE);
-	        curl_setopt($curl_session, CURLOPT_SSL_VERIFYHOST, FALSE);
+	        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
 	    }
 	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
 	    $fp = curl_exec($ch);
