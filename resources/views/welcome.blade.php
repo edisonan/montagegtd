d06da4d4c973e4d0412e0151a63923a1e9531a5b
accacc <accacc@126.com>
2019-11-13 00:31:16 +0800

    调整首页

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
-@section('title', '蒙太奇MontageGTD-高效你的生活')
-@section('description', '专业GTD工具，在这里你可以利用番茄工作法结合我们的待办列表来完成每一件事，每月每周每日均会提供实时统计，你还可以进行RSS阅读，订阅自己喜爱的文章，更可以推送文章到kindle阅读器！')
+@section('title', '蒙太奇 - 但行好事，用心生活')
+@section('description', '利用番茄工作法结合待办列表来高效完成每一件事，实时统计，笔记记录，RSS阅读，思维导图，订阅推送到kindle来帮助你记录更多想法，希望它可以帮你更多')
 
 @section('content')
  <div class="container">
       <div class="jumbotron text-center" style="color: white;    background-position: -1501px -266px;    background-image: url('/img/index_background.jpg');">
-        <h3 style="padding-bottom: 20px;">Montage GTD</h3>
-        <p class="lead"  style="padding-bottom: 100px;">专业GTD工具，在这里高效完成每一件事情，记录每一个想法，利用碎片化时间，提升一切内容。</p>
+        <h3 style="padding-bottom: 20px;">蒙太奇 - 但行好事，用心生活</h3>
+        <p class="lead"  style="padding-bottom: 100px;">不止于GTD，陪伴你做每一件事，绘制更多自己的精彩瞬间。</p>
         <p>
 	        <a class="btn btn-lg btn-primary" href="{{url('/login')}}" role="button">现在就开始吧</a>
         </p>
       </div>
-      
+      <!-- 
       <div class="row">
         <div class="col-md-3">
           <img alt="" src="/img/time-is-money.png" class="">
@@ -47,6 +47,7 @@
         </div>
         
       </div>
+       -->
 	  
     </div> <!-- /container -->
 @endsection
