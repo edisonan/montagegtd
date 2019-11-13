67c023828f3d9d23530f9d104425b59d3b76ba3e
accacc <accacc@126.com>
2019-11-13 17:36:39 +0800

    change code

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -312,7 +312,7 @@ $(document).ready(function () {
 					$str += '<a href="javascript:void(0)" class="top_task" task_value="'+data.id+'" task_is_top="' + data.is_top + '">[置顶]</a>';
 					$str += '<a href="javascript:void(0)" class="update_task" task_value="'+data.id+'">[更新]</a>';
 					$str += '<a href="javascript:void(0)" class="finish_task"  task_type="delete" task_value="'+data.id+'">[删除]</a>';
-					$str += '<a href="/notes?add_content=%23记录待办%23"'+encodeURIComponent(data.name)+'&task_id='+data.id+'" target="_blank">[记录]</a>';
+					$str += '<a href="/notes?add_content=%23记录待办%23'+encodeURIComponent(data.name)+'&task_id='+data.id+'" target="_blank">[记录]</a>';
 					$str += data.name;
 					$str += '</p>';
 					$str += '</li>'
@@ -338,7 +338,7 @@ $(document).ready(function () {
 					$str += (new Date(data.created_at)).format("hh:mm") +' - '+ (new Date(data.updated_at)).format("hh:mm");
 					$str += '</span>';
 					$str += '<p>';
-					$str += '<a href="/notes?add_content=%23记录番茄%23"'+encodeURIComponent(data.name)+'&task_id='+data.id+'" target="_blank">[记录]</a>';
+					$str += '<a href="/notes?add_content=%23记录番茄%23'+encodeURIComponent(data.name)+'&task_id='+data.id+'" target="_blank">[记录]</a>';
 					$str += data.name;
 					$str += '</p>';
 					$str += '</li>';
@@ -375,7 +375,7 @@ $(document).keyup(function(event){
 						$str += '<a href="javascript:void(0)" class="top_task" task_value="'+data.id+'" task_is_top="' + data.is_top + '">[置顶]</a>';
 						$str += '<a href="javascript:void(0)" class="update_task" task_value="'+data.id+'">[更新]</a>';
 						$str += '<a href="javascript:void(0)" class="finish_task" task_type="delete" task_value="'+data.id+'">[删除]</a>';
-						$str += '<a href="/notes?add_content=%23记录待办%23"'+encodeURIComponent(data.name)+'&task_id='+data.id+'" target="_blank">[记录]</a>';
+						$str += '<a href="/notes?add_content=%23记录待办%23'+encodeURIComponent(data.name)+'&task_id='+data.id+'" target="_blank">[记录]</a>';
 						$str += data.name;
 						$str += '</p>';
 						$str += '</li>'
@@ -408,7 +408,7 @@ $(document).keyup(function(event){
 						$str += (new Date(result_arr.result.active_pomo.created_at)).format("hh:mm") +' - '+ (new Date(result_arr.result.active_pomo.updated_at)).format("hh:mm");
 						$str += '</span>';
 						$str += '<p>';
-						$str += '<a href="/notes?add_content=%23记录番茄%23"'+encodeURIComponent(result_arr.result.active_pomo.name)+'&task_id='+result_arr.result.active_pomo.id+'" target="_blank">[记录]</a>';
+						$str += '<a href="/notes?add_content=%23记录番茄%23'+encodeURIComponent(result_arr.result.active_pomo.name)+'&task_id='+result_arr.result.active_pomo.id+'" target="_blank">[记录]</a>';
 						$str += result_arr.result.active_pomo.name;
 						$str += '</p>';
 						$str += '</li>';
