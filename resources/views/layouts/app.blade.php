32eb13497812192b47368ed11765d8e73c73a012
accacc <accacc@126.com>
2022-10-17 23:21:11 +0800

    调整js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -161,12 +161,12 @@
 	</div>
 	
 		 <script src="/js/jquery.min.js?version=3.2.1"  crossorigin="anonymous"></script>
-		<script src="/js/popper.min.js?version=1.11.0" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="/js/popper.min.js?version=1.11.0" crossorigin="anonymous"></script>
 		
 	    @yield('content')
 	
 	    <!-- JavaScripts -->
-	     <script src="/js/bootstrap.min.js?version=4.0.0-beta" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+	     <script src="/js/bootstrap.min.js?version=4.0.0-beta" crossorigin="anonymous"></script>
 	    
 	    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 		<script>
